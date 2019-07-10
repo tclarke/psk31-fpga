4921364fded3122741da824f97629b0ef303b2b2
Trevor R.H. Clarke <tclarke@ball.com>
2019-07-10 13:50:01 -0400

    Add inverted (180 phase shifted) sine wave.

--- src/dds_sine.v
+++ src/dds_sine.v
@@ -8,17 +8,20 @@ module dds_sine (
         input clk,
         input rst,
         input [N_TUNING-1:0] tuning_word,
-        output reg signed [9:0] sine
+        output reg signed [N_OUT-1:0] sine,
+        output reg signed [N_OUT-1:0] sine_i
 );
 parameter SIN_LUT_FILE = "/Users/tclarke/embedded/psk31/src/sine_lut.hex";
 parameter N_ACCUM = 14;
 parameter N_TUNING = 10;
+parameter N_OUT = 16;
 
 reg [N_ACCUM-1:0] accum = 0;
 reg signed [15:0] lut[0:255];
 /* verilator lint_off UNUSED */
 reg signed [15:0] sine_1sym;
 reg signed [15:0] sine_2sym;
+reg signed [15:0] sine_2sym_i;
 reg accum_high;
 /* verilator lint_on UNUSED */
 
@@ -31,7 +34,9 @@ always @ (posedge clk) begin
     else      accum <= accum + {3'b0, tuning_word};
     {sine_1sym, accum_high} <= {accum[N_ACCUM-2] ? lut[~accum[N_ACCUM-3:N_ACCUM-10]] : lut[accum[N_ACCUM-3:N_ACCUM-10]], accum[N_ACCUM-1]};
     sine_2sym <= accum_high ? -sine_1sym : sine_1sym;
-    sine <= sine_2sym[15:6];
+    sine_2sym_i <= accum_high ? sine_1sym : -sine_1sym;
+    sine <= sine_2sym[15:16-N_OUT];
+    sine_i <= sine_2sym_i[15:16-N_OUT];
 end
 
 endmodule
---
+++ synthesis/psk31_top.v
@@ -0,0 +1,15 @@
+//
+// Author: tclarke
+// Licensed under the GNU Public License (GPL) v3
+//
+
+
+module psk31_top (
+        input clk,
+        input rst
+);
+
+dds_sine #(.
+
+endmodule
+
